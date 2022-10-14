3462a6ba260247895c626535bf890c96e019737d
shahdahmmed <94620094+shahdahmmed@users.noreply.github.com>
2022-10-14 21:09:44 +0200

    Update BigDecimal.cpp

--- BigDecimal.cpp
+++ BigDecimal.cpp
@@ -142,6 +142,10 @@ bool BigDecimal::operator == (BigDecimal n) const
         return false;
     }
 }
+void BigDecimal:: operator=(BigDecimal n)
+{
+           num=n.num;
+}
 int BigDecimal::Sign(BigDecimal n) const
 {
     if(n.num[0]=='-')
