0043e5cb0a53a0a48d235111844b8b45801f3522
shahdahmmed <94620094+shahdahmmed@users.noreply.github.com>
2022-10-22 19:49:41 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
 #include <bits/stdc++.h>
-#include "BigDecimal.h"
-
 using namespace std;
+string modifiy(string s){
+    string c="";
+    int index=0;
+    for(int j=0;j<s.size();j++) {
+        if (isalpha(s[index++])) {
+            for (int i = index - 1; i < s.size(); ++i) {
+                if (s[i] == ' ' && s[i - 1] == ' ') {
+                    continue;
+                } else c += s[i];
+            }
+            break;
+        }
+    }
+        transform(c.begin(),c.end(),c.begin(),::tolower);
+    c[0]=toupper(c[0]);
 
-int main()
-{
-    BigDecimal num1(5);
-    BigDecimal num2("3");
-    cout <<num1 + num2 << endl;
-    return 0;
+    return c;
+}
+
+int main() {
+   string s;
+    getline(cin,s);
+    cout<<modifiy(s);
+
 }
